574abe6976f6785ea12da8d2d9f261a2a80d7882
Adriel Teles <adrielteles@outlook.com>
2023-09-27 01:28:36 -0300

    initial project

---
+++ PassMystic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassMystic
+//
+//  Created by Adriel Teles on 27/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
